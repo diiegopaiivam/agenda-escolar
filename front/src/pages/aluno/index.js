1a6017d9634c1c1a35f7a173254e473eb81230dc
Diego Paiva <diego.paiva@imobsoft.com.br>
2020-06-20 22:20:06 -0300

    funcionalidades do front

--- front/src/pages/aluno/index.js
+++ front/src/pages/aluno/index.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FiLogOut, FiPlus, FiSearch, FiTrash, FiEdit } from 'react-icons/fi';
 import { Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
 import AlunoImg from './../../assets/aluno.svg';
 
+import api from './../../services/api';
+
 import './style.css';
 
 const Aluno = () => {
+
+    const [series, setSeries ] = useState([]);
+    const [name, setName] = useState('');
+    const [responsavel, setResponsavel] = useState('');
+    const [alunos, setAlunos] = useState([]);
+    const [serie_id, setSerieID] = useState();
+    
+    useEffect(() => {
+        api.get('/serie').then(response => {
+            setSeries(response.data);
+        })
+    },[]);
+
+    async function handleSubmit(){
+        api.get(`/series?serie_id=${serie_id}&responsavel=${responsavel}&name=${name}&page=1`).then(response => {
+            setAlunos(response.data);
+        });
+        
+    }
+
+    function handleOnChange(e) {
+        e.target.name = e.target.value;
+        setName(e.target.name);
+    };
+
+    function handleOnChangeResponsavel(e){
+        e.target.name = e.target.value;
+        setResponsavel(e.target.name);
+    }
+
+    function handleSelectSerie(e){
+        setSerieID(e.target.value);
+    }
+    
     return ( 
         <div className="home">
             <div className="container">
@@ -20,20 +56,21 @@ const Aluno = () => {
                 </header>
                 <section>
                     <div className="title">Alunos</div>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="row">
                             <div className="form-group">
-                                <select>
-                                    <option value="1">1ª Serie</option>
-                                    <option value="2">2ª Serie</option>
-                                    <option value="3">3ª Serie</option>
+                                <select onChange={handleSelectSerie} value={serie_id}>
+                                    <option value="0">Selecione uma Série</option>
+                                    {series.map(serie => (
+                                        <option key={serie.id} value={serie.id}>{serie.serie}</option>
+                                    ))}
                                 </select>
-                                <input type="text" className="name" placeholder="Nome do Aluno" />
-                                <input type="text" placeholder="Nome do Responsável" />
+                                <input type="text" onChange={handleOnChange} name="name" value={name} className="name" placeholder="Nome do Aluno" />
+                                <input type="text" onChange={handleOnChangeResponsavel} name="responsavel" value={responsavel} placeholder="Nome do Responsável" />
                             </div>
                             <div className="adicionais">
                                 <button><FiPlus size={35} color="#4CB0E3" title="Cadastrar Aluno" /></button>
-                                <button><FiSearch size={30} color="#4CB0E3" title="Pesquisar Aluno" /></button>
+                                <button type="submit"><FiSearch size={30} color="#4CB0E3" title="Pesquisar Aluno" /></button>
                             </div>
                         </div>
                     </form>
